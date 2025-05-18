fecde90c27ccf4a382185706e16d56721205b221
hashmishaikhmxl <hashmi.shaikh@mexcelle.com>
2025-05-18 21:02:54 +0530

    added the new file

--- server/controllers/userController.js
+++
@@ -1,6 +0,0 @@
-import Product from '../models/Product.js';
-
-export const getUserListings = async (req, res) => {
-  const listings = await Product.find({ seller: req.user._id });
-  res.json(listings);
-};
--- server/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getUserListings } from '../controllers/userController.js';
-import { protect } from '../middleware/authMiddleware.js';
-
-const router = express.Router();
-
-router.get('/my-listings', protect, getUserListings);
-
-export default router;
